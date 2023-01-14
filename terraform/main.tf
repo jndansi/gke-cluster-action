5346d7c88bee28e9bdd87655060969117f25161b
jason <jndansi@gmail.com>
2023-01-14 13:54:30 -0500

    Newest change

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = var.machine-type
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
+    service_account = google_service_account.default[each.value].email
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
