33aae5f04802e3f00c9dbf83ba98be93c082fa95
jason <jndansi@gmail.com>
2023-01-14 11:56:57 -0500

    main.tf change

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "e2-medium"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = "terraform-key@nimble-album-369317.iam.gserviceaccount.com"
+    service_account = "terraform@nimble-album-369317.iam.gserviceaccount.com"
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
